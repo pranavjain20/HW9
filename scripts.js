4e148c3a5f5779730f670d74328bd92c620e67e1
pranavjain20 <60300212+pranavjain20@users.noreply.github.com>
2021-03-26 13:14:12 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,13 +18,9 @@ function onLoad() {
 
             document.getElementById("messages").innerHTML = "Whoops, something went terribly wrong!";
 
-        } else if (request.readyState == 3) {
+        } 
 
-            document.getElementById("messages").innerHTML = "Come back soon!";
-
-        }
-
-            }//end on readystate change
+            }
 
         // Step 4: fire off the HTTP request
             request.send();
