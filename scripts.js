bb77758d5003231a95bed9e640252920af4cd048
pranavjain20 <60300212+pranavjain20@users.noreply.github.com>
2021-03-26 13:09:30 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,22 +11,11 @@ function onLoad() {
             result = request.responseText;
             data = JSON.parse(result);
 
-            document.getElementById("json_str").innerHTML = JSON.stringify(data);
+            document.getElementById("json_str").innerHTML = JSON.stringify(cover_list);
 
-
-        } else if (request.readyState == 4 && request.status != 200) {
-
-            document.getElementById("messages").innerHTML = "Whoops, something went terribly wrong!";
-
-        } else if (request.readyState == 3) {
-
-            document.getElementById("messages").innerHTML = "Come back soon!";
 
         }
 
-            }//end on readystate change
-
-        // Step 4: fire off the HTTP request
             request.send();
             console.log("4 request sent");
 
