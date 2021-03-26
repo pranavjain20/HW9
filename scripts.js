b82d879a8e09ca95401f756ab16bc00d8e0be5b7
pranavjain20 <60300212+pranavjain20@users.noreply.github.com>
2021-03-26 13:13:25 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function onLoad() {
 
     request = new XMLHttpRequest();
 
-    request.open("GET", "data.json", true);
+    request.open("GET", "cover_list.json", true);
 
     request.onreadystatechange = function() {
 
@@ -14,9 +14,19 @@ function onLoad() {
             document.getElementById("json_str").innerHTML = JSON.stringify(cover_list);
 
 
-        }
-    }
+        } else if (request.readyState == 4 && request.status != 200) {
 
+            document.getElementById("messages").innerHTML = "Whoops, something went terribly wrong!";
+
+        } else if (request.readyState == 3) {
+
+            document.getElementById("messages").innerHTML = "Come back soon!";
+
+        }
+
+            }//end on readystate change
+
+        // Step 4: fire off the HTTP request
             request.send();
             console.log("4 request sent");
 
