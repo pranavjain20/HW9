33a482c282bd0a101b592b0fc4e6b345126c9d44
pranavjain20 <60300212+pranavjain20@users.noreply.github.com>
2021-03-26 20:56:27 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,33 @@
-function onLoad() {
+// function onLoad() {
 
-    request = new XMLHttpRequest();
+//     request = new XMLHttpRequest();
 
-    request.open("GET", "https://pranavjain20.github.io/HW9/cover_list.json", true);
+//     request.open("GET", "https://pranavjain20.github.io/HW9/cover_list.json", true);
 
-    request.onreadystatechange = function() {
+//     request.onreadystatechange = function() {
 
-        if (request.readyState == 4 && request.status == 200) {
+//         if (request.readyState == 4 && request.status == 200) {
 
-            result = request.responseText;
-//             document.write(result);
-//             cover_list = JSON.parse(result);
+//             result = request.responseText;
+// //             document.write(result);
+// //             cover_list = JSON.parse(result);
 
-//             document.getElementById("json_str").innerHTML = JSON.stringify(cover_list);
-        }
-            }
-            request.send();
-            console.log("4 request sent");
+// //             document.getElementById("json_str").innerHTML = JSON.stringify(cover_list);
+//         }
+//             }
+//             request.send();
+//             console.log("4 request sent");
 
-}
+// }
+
+$(document).ready(function(){
+    console.log('1');
+    $.get("https://pranavjain20.github.io/HW9/cover_list.json",
+    function(cover_list){
+        console.log('2');
+        str = JSON.stringify(cover_list);
+        console.log("asdf");
+        $("#json_str").value("asdf");
+    })
+
+});
