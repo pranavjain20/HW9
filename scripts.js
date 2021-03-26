4632956f7f09258be4ae168379551d0dd56e2ecb
pranavjain20 <60300212+pranavjain20@users.noreply.github.com>
2021-03-26 09:45:52 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,33 @@
+function onLoad() {
+
+    request = new XMLHttpRequest();
+
+    request.open("GET", "data.json", true);
+
+    request.onreadystatechange = function() {
+
+        if (request.readyState == 4 && request.status == 200) {
+
+            result = request.responseText;
+            data = JSON.parse(result);
+
+            document.getElementById("json_str").innerHTML = JSON.stringify(data);
+
+
+        } else if (request.readyState == 4 && request.status != 200) {
+
+            document.getElementById("messages").innerHTML = "Whoops, something went terribly wrong!";
+
+        } else if (request.readyState == 3) {
+
+            document.getElementById("messages").innerHTML = "Come back soon!";
+
+        }
+
+            }//end on readystate change
+
+        // Step 4: fire off the HTTP request
+            request.send();
+            console.log("4 request sent");
+
+}
